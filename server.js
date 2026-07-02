1dcc06ca97d00f424467421fe212daecaa37497e
Sebasbehe <sebastianpersmor2006@gmail.com>
2026-07-02 16:46:43 -0500

    dependencies

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const bodyParser = require("body-parser");
 const bcrypt = require("bcryptjs");
 const { Pool } = require("pg");
 const path = require("path");
+const transporter = require("./utils/mail");
 
 const app = express();
 app.set("trust proxy", 1);
@@ -579,6 +580,36 @@ app.get("/api/history", requireLogin, (req, res) => {
       res.status(500)
         .send(err.message);
     });
+});
+app.get("/test-email", async (req, res) => {
+
+    try {
+
+        await transporter.sendMail({
+
+            from: `"Sistema de Reservas" <${process.env.EMAIL_USER}>`,
+
+            to: process.env.EMAIL_USER,
+
+            subject: "Prueba de correo",
+
+            html: `
+                <h2>Correo de prueba</h2>
+                <p>Si recibiste este correo significa que Gmail está configurado correctamente.</p>
+            `
+
+        });
+
+        res.send("Correo enviado correctamente.");
+
+    } catch (err) {
+
+        console.error(err);
+
+        res.status(500).send(err.message);
+
+    }
+
 });
 // ---------------------------
 // 🔹 Iniciar servidor
